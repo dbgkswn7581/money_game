decce46e9a81503db09b12ef90020a12fdbc7491
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-12-05 23:44:29 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,26 +19,6 @@ async def on_ready():
     await client.change_presence(status = discord.Status.online, activity = game)
 
 
-# db_url = 'https://moneygame-34830-default-rtdb.firebaseio.com/'
-
-# cred = credentials.Certificate({
-#   "type": "service_account",
-#   "project_id": "moneygame-34830",
-#   "private_key_id": "cd18119563a0eafff8d2e124bbcbee364b116b5b",
-#   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDIB/0SO+D7GHPF\nioolG1ktlYe18s1ssyA3cfAyJlnM+ojakAcc5ztwL8VQo/7WskAYov3Ex0VxWt/W\noH54ymS/y1BDGKCl8npkzBVZIiiZ6nrBnbP1HINWOPfIRuQYeoX2PyjXK8+2mhs1\nyssROIFAcNxDVw21qPJNgLjbKgFabFOcOExfGTkOJfOV3c50zM6pg3q021fqs4dB\ngfEqP5YmTTxtomIz+kjNRTL493YsEwUOTFeTPf4mo4zX6Mvn81QluA4tAPbOAbUo\nKaWzv23T5bOBqJtkkAzgBSfOSeZsdfl6q5GyqzHC0v+tMX3J6u3KQyJqMJB5Zu9a\n37Rh2tDLAgMBAAECggEAQWMTH9XBCnOIzKcn109kTFlX3ms7KXA4dMdi/BG7Qx0W\nhVOVb6ZKsLZ86ophMG9eMm99Qsjc81wAVZlrHjMS0fs1BmnTgcuMpMxtohBfc0jJ\nzeauP86NRC8lGCvMPhA3IDKvN/8TTB5+DIx46u0smxMJfV3EloBejGUqiEHn5VV1\n2QSr+n8O8YFrZbVRVQmoZEpCuyvFp29qLrpT3py28vAs2mVRIoZU3FEwbze79xRg\nhlfUZgywQwDfDN5HqCYNT/sYrIFTjoNqg7u9L9PDFIP+5naEti2HErr+bRgMI8W0\nG8HOJH0r667DWyklHaq3PxXJVSssRJ3mGaT5kJqiUQKBgQDnIybDGPfeQDBuTl+/\noiHTFD8hSQ1WkLle20yOMZXkv75PFrVnJfg8CVKp8HsQEhl2WQG7Zf+8hR5i7ZPH\njrjfto+IdPGrqOsBLG8Vi/KG4ASBLbe6pG78WvruI9mlZloojvaopkIEO8hSH93Z\nM+DdKKfkI8HNlFHQHy/05YcLwwKBgQDdjEP8+0nV1SkStxtRAisaY+UJo6blUJiF\nUakpK8bqWG6kLRyFwfQ+Rww68s6ZYVKl0oc/Dbf5GXhstKx1iyuACXwXWwruXmZu\nflxUAWgJBKTw3I7EqQPzroprqZ7Im6+Tk7i2Ka8dbtLDtTWGs7Ne0i6OGaOM2Tnx\nUM0Fvk++WQKBgCJkwKfXV0rjElHXp7fEUHQwVxSE4k83Sd4rk8C08DERtyfdoTCw\nHbgRSxOujSFfEBrkM7XG/771sAfYglz3h9sCN2l+vVdl01JhYkY6hOQ9AW6Xdvd2\n8GzsvNoiy4gpKtFONLXzx28J1k0iIMjpR1ShcBoql/QXVfU6LtCqNuYxAoGBAKQW\nDgjhqMbqEHifC2lNPNVX5TvlbGfs8HnViH9IwBOzGYcZLkXW5+n0Pxet29x323mb\nWrYVmAuzlKCWlCCeycITxEecE5WGb0Eo74L8Y6xCN1N5V3hKYfXqcPTadRLJVBnu\nT+EDMIKRISL4JEO29oOlEfwG+z4HaUXUfXTihjdJAoGAdmgCbrA4ll5uUkSW2/j+\nJglBqeEHIO5XjPeEB23XzO8o/kIl8PqVU7ZE0x3uCiLd41JxVG/3iFt1r7sKtRYf\nRX4hgi8wpAXGGzJ5QEY0VPYhvsIZNwef3xDKEEDV67BUKAfLQsbsRhn/I18DpuW8\n06GDHpzjVWYw4pMcsImqgFs=\n-----END PRIVATE KEY-----\n",
-#   "client_email": "firebase-adminsdk-9nu82@moneygame-34830.iam.gserviceaccount.com",
-#   "client_id": "115266264813716255876",
-#   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-#   "token_uri": "https://oauth2.googleapis.com/token",
-#   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-#   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9nu82%40moneygame-34830.iam.gserviceaccount.com"
-# }
-# )
- 
-# firebase_admin.initialize_app(cred,{
-#     'databaseURL' : db_url
-# }) 
-
 @client.command(name="가입")
 async def signup(ctx, *text):
   user_keys = []
@@ -747,38 +727,68 @@ async def reg(ctx, *text):
               await ctx.send('잘못된 회사명입니다.\n`$주식`을 통해 회사명을 확인해주십시오')
             
             else:
-              try:
-                amount = int(amount)
-                if amount <= 0:
-                  await ctx.send('수량 부분에 자연수만 입력해주십시오.')
+              if amount == '다' or amount == 'ㄷ' or amount == 'all' or amount == 'e':
+                com = find_company(company)
+                kr_com = kr_company(com)
+
+                ref = db.reference()
+                dic = ref.get()
+                data = dic[str(user_id)]
+                money = data['money']
+                taked = data[com]['amount']
+                now_num = find_now_num(dic[com])
+                now_value = dic[com][now_num]['value']
+
+                amount = int(money / now_value)
+
+                if amount == 0:
+                  await ctx.send('보유 금액이 부족합니다.')
+
                 else:
-                  com = find_company(company)
-                  kr_com = kr_company(com)
-
                   ref = db.reference()
-                  dic = ref.get()
-                  data = dic[str(user_id)]
-                  money = data['money']
-                  taked = data[com]['amount']
-                  now_num = find_now_num(dic[com])
-                  now_value = dic[com][now_num]['value']
                   
+                  ref.child(str(user_id)).child(com).child('value').set(now_value)
+                  ref.child(str(user_id)).child(com).child('amount').set(amount + taked)
+                  ref.child(str(user_id)).child('money').set((money-(amount*now_value)))
 
-                  if (amount * now_value) > money:
-                    await ctx.send('보유 금액이 부족합니다.')
+                  await ctx.send(
+                    '주식을 구매했어요.\n`- %s 코인`\n`+ %s %s 주`'
+                    %(replace_amount(amount*now_value), kr_com, replace_amount(amount))
+                    )
+
+              else:
+                try:
+                  amount = int(amount)
+                  if amount <= 0:
+                    await ctx.send('수량 부분에 자연수만 입력해주십시오.')
                   else:
+                    com = find_company(company)
+                    kr_com = kr_company(com)
+
                     ref = db.reference()
                     dic = ref.get()
+                    data = dic[str(user_id)]
+                    money = data['money']
+                    taked = data[com]['amount']
+                    now_num = find_now_num(dic[com])
+                    now_value = dic[com][now_num]['value']
                     
-                    ref.child(str(user_id)).child(com).child('value').set(now_value)
-                    ref.child(str(user_id)).child(com).child('amount').set(amount + taked)
-                    ref.child(str(user_id)).child('money').set((money-(amount*now_value)))
-                    await ctx.send(
-                      '주식을 구매했어요.\n`- %s 코인`\n`+ %s %s 주`'
-                      %(replace_amount(amount*now_value), kr_com, replace_amount(amount))
-                      )
-              except:
-                await ctx.send('수량 부분이 자연수가 아니거나 오류가 발생했습니다.\n```diff\n+++ 오류 발생 시 개발자에게 문의 바랍니다.```')
+
+                    if (amount * now_value) > money:
+                      await ctx.send('보유 금액이 부족합니다.')
+                    else:
+                      ref = db.reference()
+                      dic = ref.get()
+                      
+                      ref.child(str(user_id)).child(com).child('value').set(now_value)
+                      ref.child(str(user_id)).child(com).child('amount').set(amount + taked)
+                      ref.child(str(user_id)).child('money').set((money-(amount*now_value)))
+                      await ctx.send(
+                        '주식을 구매했어요.\n`- %s 코인`\n`+ %s %s 주`'
+                        %(replace_amount(amount*now_value), kr_com, replace_amount(amount))
+                        )
+                except:
+                  await ctx.send('수량 부분이 자연수가 아니거나 오류가 발생했습니다.\n```diff\n+++ 오류 발생 시 개발자에게 문의 바랍니다.```')
 
     elif order == '판매' or order == 'ㅍㅁ' or order == 'va':
       ref = db.reference()
@@ -806,35 +816,57 @@ async def reg(ctx, *text):
               await ctx.send('잘못된 회사명입니다.\n`$주식`을 통해 회사명을 확인해주십시오')
             
             else:
-              try:
-                amount = int(amount)
-                if amount <= 0:
-                  await ctx.send('수량 부분에 자연수만 입력해주십시오.')
-                else:
-                  com = find_company(company)
-                  kr_com = kr_company(com)
+              if amount == '다' or amount == 'ㄷ' or amount == 'all' or amount == 'e':
+                com = find_company(company)
+                kr_com = kr_company(com)
 
-                  ref = db.reference()
-                  dic = ref.get()
-                  data = dic[str(user_id)]
-                  money = data['money']
-                  taked = data[com]['amount']
-                  now_num = find_now_num(dic[com])
-                  now_value = dic[com][now_num]['value']
+                ref = db.reference()
+                dic = ref.get()
+                data = dic[str(user_id)]
+                money = data['money']
+                taked = data[com]['amount']
+                now_num = find_now_num(dic[com])
+                now_value = dic[com][now_num]['value']
 
-                  if amount > taked:
-                    await ctx.send('보유 수량이 입력하신 판매 수량보다 적습니다.')
-                  else:
-                    ref = db.reference()
-                    dic = ref.get()
-                    ref.child(str(user_id)).child(com).child('amount').set(taked - amount)
-                    ref.child(str(user_id)).child('money').set((money+(amount*now_value)))
-                    await ctx.send(
-                      '주식을 판매했어요.\n`+ %s 코인`\n`- %s %s 주`'
+                amount = taked
+
+                ref.child(str(user_id)).child(com).child('amount').set(taked - amount)
+                ref.child(str(user_id)).child('money').set((money+(amount*now_value)))
+
+                await ctx.send(
+                  '주식을 판매했어요.\n`+ %s 코인`\n`- %s %s 주`'
                       %(replace_amount(amount*now_value), kr_com, replace_amount(amount))
                       )
-              except:
-                await ctx.send('수량 부분이 자연수가 아니거나 오류가 발생했습니다.\n```diff\n+++ 오류 발생 시 개발자에게 문의 바랍니다.```')
+              else:
+                try:
+                  amount = int(amount)
+                  if amount <= 0:
+                    await ctx.send('수량 부분에 자연수만 입력해주십시오.')
+                  else:
+                    com = find_company(company)
+                    kr_com = kr_company(com)
+
+                    ref = db.reference()
+                    dic = ref.get()
+                    data = dic[str(user_id)]
+                    money = data['money']
+                    taked = data[com]['amount']
+                    now_num = find_now_num(dic[com])
+                    now_value = dic[com][now_num]['value']
+
+                    if amount > taked:
+                      await ctx.send('보유 수량이 입력하신 판매 수량보다 적습니다.')
+                    else:
+                      ref = db.reference()
+                      dic = ref.get()
+                      ref.child(str(user_id)).child(com).child('amount').set(taked - amount)
+                      ref.child(str(user_id)).child('money').set((money+(amount*now_value)))
+                      await ctx.send(
+                        '주식을 판매했어요.\n`+ %s 코인`\n`- %s %s 주`'
+                        %(replace_amount(amount*now_value), kr_com, replace_amount(amount))
+                        )
+                except:
+                  await ctx.send('수량 부분이 자연수가 아니거나 오류가 발생했습니다.\n```diff\n+++ 오류 발생 시 개발자에게 문의 바랍니다.```')
                
 @client.command(name="wt")
 async def reg(ctx, *text):
