90b8ba152d4576575a440368de0908c0efe1a3e0
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-12-06 09:18:36 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1514,63 +1514,171 @@ async def help(ctx):
   await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n`$돈` : 본인 정보를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
 
 ##############################################################################################################################
-
 @client.command(name='돈')
-async def myself(ctx):
-  ref = db.reference()
-  dic = ref.get()
+async def myself(ctx, *text):
+  if len(text) == 0:
+    ref = db.reference()
+    dic = ref.get()
 
-  user_id = ctx.author.id
+    user_id = ctx.author.id
 
-  money = dic[str(user_id)]['money']
-  nick = dic[str(user_id)]['nickname']
-  amount = dic[str(user_id)]['ce']['amount'] + dic[str(user_id)]['go']['amount'] + dic[str(user_id)]['mk']['amount'] + dic[str(user_id)]['nl']['amount'] + dic[str(user_id)]['pd']['amount'] + dic[str(user_id)]['sl']['amount'] + dic[str(user_id)]['sn']['amount'] + dic[str(user_id)]['tc']['amount'] + dic[str(user_id)]['ua']['amount'] + dic[str(user_id)]['pg']['amount']
+    money = dic[str(user_id)]['money']
+    nick = dic[str(user_id)]['nickname']
+    amount = dic[str(user_id)]['ce']['amount'] + dic[str(user_id)]['go']['amount'] + dic[str(user_id)]['mk']['amount'] + dic[str(user_id)]['nl']['amount'] + dic[str(user_id)]['pd']['amount'] + dic[str(user_id)]['sl']['amount'] + dic[str(user_id)]['sn']['amount'] + dic[str(user_id)]['tc']['amount'] + dic[str(user_id)]['ua']['amount'] + dic[str(user_id)]['pg']['amount']
 
-  embed = discord.Embed(
-  color = discord.Color.blue()
-  )
-  embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
-  embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
-  embed.set_footer(text='%s님의 정보' %nick)
-  await ctx.send(embed=embed)
+    embed = discord.Embed(
+    color = discord.Color.blue()
+    )
+    embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
+    embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
+    embed.set_footer(text='%s님의 정보' %nick)
+    await ctx.send(embed=embed)
+  elif len(text) == 1:
+    nick = str(text[0])
+    ref = db.reference()
+    dic = ref.get()
+    del dic['pg']
+    del dic['sn']
+    del dic['mk']
+    del dic['ua']
+    del dic['nl']
+    del dic['pd']
+    del dic['go']
+    del dic['tc']
+    del dic['sl']
+    del dic['ce']
+    del dic['admin']
 
-@client.command(name='e')
-async def myself(ctx):
-  ref = db.reference()
-  dic = ref.get()
+    money = str()
+    amount = str()
 
-  user_id = ctx.author.id
-
-  money = dic[str(user_id)]['money']
-  nick = dic[str(user_id)]['nickname']
-  amount = dic[str(user_id)]['ce']['amount'] + dic[str(user_id)]['go']['amount'] + dic[str(user_id)]['mk']['amount'] + dic[str(user_id)]['nl']['amount'] + dic[str(user_id)]['pd']['amount'] + dic[str(user_id)]['sl']['amount'] + dic[str(user_id)]['sn']['amount'] + dic[str(user_id)]['tc']['amount'] + dic[str(user_id)]['ua']['amount'] + dic[str(user_id)]['pg']['amount']
-
-  embed = discord.Embed(
-  color = discord.Color.blue()
-  )
-  embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
-  embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
-  embed.set_footer(text='%s님의 정보' %nick)
-  await ctx.send(embed=embed)
+    user_ids = list(dic.keys())
+    for i in user_ids:
+      if nick == dic[str(i)]['nickname']:
+        money = dic[str(i)]['money']
+        amount = dic[str(i)]['ce']['amount'] + dic[str(i)]['go']['amount'] + dic[str(i)]['mk']['amount'] + dic[str(i)]['nl']['amount'] + dic[str(i)]['pd']['amount'] + dic[str(i)]['sl']['amount'] + dic[str(i)]['sn']['amount'] + dic[str(i)]['tc']['amount'] + dic[str(i)]['ua']['amount'] + dic[str(i)]['pg']['amount']
+    
+    if money == '' and amount == '':
+      await ctx.send('입력하신 닉네임은 존재하지 않는 유저ㅇ입니다.')
+    else:
+      embed = discord.Embed(
+      color = discord.Color.blue()
+      )
+      embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
+      embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
+      embed.set_footer(text='%s님의 정보' %nick)
+      await ctx.send(embed=embed)
 
 @client.command(name='ㄷ')
-async def myself(ctx):
-  ref = db.reference()
-  dic = ref.get()
+async def myself(ctx, *text):
+  if len(text) == 0:
+    ref = db.reference()
+    dic = ref.get()
 
-  user_id = ctx.author.id
+    user_id = ctx.author.id
 
-  money = dic[str(user_id)]['money']
-  nick = dic[str(user_id)]['nickname']
-  amount = dic[str(user_id)]['ce']['amount'] + dic[str(user_id)]['go']['amount'] + dic[str(user_id)]['mk']['amount'] + dic[str(user_id)]['nl']['amount'] + dic[str(user_id)]['pd']['amount'] + dic[str(user_id)]['sl']['amount'] + dic[str(user_id)]['sn']['amount'] + dic[str(user_id)]['tc']['amount'] + dic[str(user_id)]['ua']['amount'] + dic[str(user_id)]['pg']['amount']
+    money = dic[str(user_id)]['money']
+    nick = dic[str(user_id)]['nickname']
+    amount = dic[str(user_id)]['ce']['amount'] + dic[str(user_id)]['go']['amount'] + dic[str(user_id)]['mk']['amount'] + dic[str(user_id)]['nl']['amount'] + dic[str(user_id)]['pd']['amount'] + dic[str(user_id)]['sl']['amount'] + dic[str(user_id)]['sn']['amount'] + dic[str(user_id)]['tc']['amount'] + dic[str(user_id)]['ua']['amount'] + dic[str(user_id)]['pg']['amount']
 
-  embed = discord.Embed(
-  color = discord.Color.blue()
-  )
-  embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
-  embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
-  embed.set_footer(text='%s님의 정보' %nick)
-  await ctx.send(embed=embed)
+    embed = discord.Embed(
+    color = discord.Color.blue()
+    )
+    embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
+    embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
+    embed.set_footer(text='%s님의 정보' %nick)
+    await ctx.send(embed=embed)
+  elif len(text) == 1:
+    nick = str(text[0])
+    ref = db.reference()
+    dic = ref.get()
+    del dic['pg']
+    del dic['sn']
+    del dic['mk']
+    del dic['ua']
+    del dic['nl']
+    del dic['pd']
+    del dic['go']
+    del dic['tc']
+    del dic['sl']
+    del dic['ce']
+    del dic['admin']
+
+    money = str()
+    amount = str()
+
+    user_ids = list(dic.keys())
+    for i in user_ids:
+      if nick == dic[str(i)]['nickname']:
+        money = dic[str(i)]['money']
+        amount = dic[str(i)]['ce']['amount'] + dic[str(i)]['go']['amount'] + dic[str(i)]['mk']['amount'] + dic[str(i)]['nl']['amount'] + dic[str(i)]['pd']['amount'] + dic[str(i)]['sl']['amount'] + dic[str(i)]['sn']['amount'] + dic[str(i)]['tc']['amount'] + dic[str(i)]['ua']['amount'] + dic[str(i)]['pg']['amount']
+    
+    if money == '' and amount == '':
+      await ctx.send('입력하신 닉네임은 존재하지 않는 유저ㅇ입니다.')
+    else:
+      embed = discord.Embed(
+      color = discord.Color.blue()
+      )
+      embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
+      embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
+      embed.set_footer(text='%s님의 정보' %nick)
+      await ctx.send(embed=embed)
+
+@client.command(name='e')
+async def myself(ctx, *text):
+  if len(text) == 0:
+    ref = db.reference()
+    dic = ref.get()
+
+    user_id = ctx.author.id
+
+    money = dic[str(user_id)]['money']
+    nick = dic[str(user_id)]['nickname']
+    amount = dic[str(user_id)]['ce']['amount'] + dic[str(user_id)]['go']['amount'] + dic[str(user_id)]['mk']['amount'] + dic[str(user_id)]['nl']['amount'] + dic[str(user_id)]['pd']['amount'] + dic[str(user_id)]['sl']['amount'] + dic[str(user_id)]['sn']['amount'] + dic[str(user_id)]['tc']['amount'] + dic[str(user_id)]['ua']['amount'] + dic[str(user_id)]['pg']['amount']
+
+    embed = discord.Embed(
+    color = discord.Color.blue()
+    )
+    embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
+    embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
+    embed.set_footer(text='%s님의 정보' %nick)
+    await ctx.send(embed=embed)
+  elif len(text) == 1:
+    nick = str(text[0])
+    ref = db.reference()
+    dic = ref.get()
+    del dic['pg']
+    del dic['sn']
+    del dic['mk']
+    del dic['ua']
+    del dic['nl']
+    del dic['pd']
+    del dic['go']
+    del dic['tc']
+    del dic['sl']
+    del dic['ce']
+    del dic['admin']
+
+    money = str()
+    amount = str()
+
+    user_ids = list(dic.keys())
+    for i in user_ids:
+      if nick == dic[str(i)]['nickname']:
+        money = dic[str(i)]['money']
+        amount = dic[str(i)]['ce']['amount'] + dic[str(i)]['go']['amount'] + dic[str(i)]['mk']['amount'] + dic[str(i)]['nl']['amount'] + dic[str(i)]['pd']['amount'] + dic[str(i)]['sl']['amount'] + dic[str(i)]['sn']['amount'] + dic[str(i)]['tc']['amount'] + dic[str(i)]['ua']['amount'] + dic[str(i)]['pg']['amount']
+    
+    if money == '' and amount == '':
+      await ctx.send('입력하신 닉네임은 존재하지 않는 유저ㅇ입니다.')
+    else:
+      embed = discord.Embed(
+      color = discord.Color.blue()
+      )
+      embed.add_field(name=":dollar: 보유 자금", value="**%s**" %replace_amount(money), inline=False)
+      embed.add_field(name=":chart_with_upwards_trend: 총 보유 주", value="**%s**" %replace_amount(amount), inline=False)
+      embed.set_footer(text='%s님의 정보' %nick)
+      await ctx.send(embed=embed)
+
 
 
 ##############################################################################################################################
