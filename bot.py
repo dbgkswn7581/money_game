c8e87c4d06ff93b3265f42627e454b34d6df2f4d
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-12-07 13:45:35 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -488,7 +488,6 @@ async def refresh():
 
 
 ##############################################################################################################################
-
 def get_value(dic):
   for i in range(1,11):
     now = dic[i]['now']
@@ -502,6 +501,7 @@ def get_value(dic):
         now_value = format(now_value, ',d')
 
         if value_gap > 0 :
+
           re = {'value' : now_value, 'pm' : '+', 'gap' : '▲'+str(value_gap)}
           return re
         elif value_gap == 0 :
@@ -518,11 +518,7 @@ def get_value(dic):
         now_value = dic[i]['value']
         value_gap = now_value - before_value
 
-        if now_value >= 1000:
-          now_value = str(now_value)
-          now_value = now_value[0:1] + ',' + now_value[1:]
-        else:
-          now_value = str(now_value)
+        now_value = format(now_value, ',d')
 
         if value_gap > 0 :
           re = {'value' : now_value, 'pm' : '+', 'gap' : '▲'+str(value_gap)}
