33dfc4a218fa9532ed249007360deb44aabf36d5
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-12-28 18:56:07 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,19 +103,19 @@ async def reg(ctx, *text):
 
 @client.command(name='도움말')
 async def help(ctx):
-  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n`$뽑기` : 뽑기 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
+  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
 
 @client.command(name='명령어')
 async def help(ctx):
-  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n`$뽑기` : 뽑기 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
+  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
 
 @client.command(name='?')
 async def help(ctx):
-  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n`$뽑기` : 뽑기 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
+  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
 
 @client.command(name='command')
 async def help(ctx):
-  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n`$뽑기` : 뽑기 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
+  await ctx.send('`$가입 (닉네임)`  : 시스템에 가입합니다.\n`$탈퇴` : 시스템에서 탈퇴합니다.\n\n`$주식` : 현재 주식 현황을 확인합니다.\n`$주식 구매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 구매합니다.\n`$주식 판매 (회사명) (수량)` : 해당 회사의 주식을 수량만큼 판매합니다.\n`$주식 나` : 현재 본인이 보유중인 주식을 확인할 수 있습니다.\n`$주식 그래프 (회사명)` : 해당 회사의 주식 변동 그래프를 확인할 수 있습니다.\n\n`$돈` : 본인 정보를 확인할 수 있습니다.\n`$순위` : 순위 관련 명령어를 확인할 수 있습니다.\n```diff\n+ 위 명령어는 자음 또는 영어로 입력할 수 있습니다.```')
 
 ##############################################################################################################################
 
@@ -148,68 +148,68 @@ async def myself(ctx, *text):
 
 ##############################################################################################################################
 
-def replace_box(step):
-  if step == '1':
-    return 1000
-  elif step == '2':
-    return 10000
-  elif step == '3':
-    return 100000
-  elif step == '4':
-    return 1000000
-  elif step == '5':
-    return 10000000
-  elif step == '6':
-    return 100000000
-  elif step == '7':
-    return 1000000000
-  elif step == '8':
-    return 10000000000
+# def replace_box(step):
+#   if step == '1':
+#     return 1000
+#   elif step == '2':
+#     return 10000
+#   elif step == '3':
+#     return 100000
+#   elif step == '4':
+#     return 1000000
+#   elif step == '5':
+#     return 10000000
+#   elif step == '6':
+#     return 100000000
+#   elif step == '7':
+#     return 1000000000
+#   elif step == '8':
+#     return 10000000000
 
-@client.command(name='뽑기')
-async def draw(ctx, *text):
-  from draw import draw
-  embed = draw(ctx, text)
-  if type(embed) == str:
-    await ctx.send(embed)
-  else:
-    await ctx.send(embed=embed)
+# @client.command(name='뽑기')
+# async def draw(ctx, *text):
+#   from draw import draw
+#   embed = draw(ctx, text)
+#   if type(embed) == str:
+#     await ctx.send(embed)
+#   else:
+#     await ctx.send(embed=embed)
 
-@client.command(name='Qr')
-async def draw(ctx, *text):
-  from draw import draw
-  embed = draw(ctx, text)
-  if type(embed) == str:
-    await ctx.send(embed)
-  else:
-    await ctx.send(embed=embed)
+# @client.command(name='Qr')
+# async def draw(ctx, *text):
+#   from draw import draw
+#   embed = draw(ctx, text)
+#   if type(embed) == str:
+#     await ctx.send(embed)
+#   else:
+#     await ctx.send(embed=embed)
 
-@client.command(name='ㅃㄱ')
-async def draw(ctx, *text):
-  from draw import draw
-  embed = draw(ctx, text)
-  if type(embed) == str:
-    await ctx.send(embed)
-  else:
-    await ctx.send(embed=embed)
+# @client.command(name='ㅃㄱ')
+# async def draw(ctx, *text):
+#   from draw import draw
+#   embed = draw(ctx, text)
+#   if type(embed) == str:
+#     await ctx.send(embed)
+#   else:
+#     await ctx.send(embed=embed)
 
-@client.command(name='ㅂㄱ')
-async def draw(ctx, *text):
-  from draw import draw
-  embed = draw(ctx, text)
-  if type(embed) == str:
-    await ctx.send(embed)
-  else:
-    await ctx.send(embed=embed)
+# @client.command(name='ㅂㄱ')
+# async def draw(ctx, *text):
+#   from draw import draw
+#   embed = draw(ctx, text)
+#   if type(embed) == str:
+#     await ctx.send(embed)
+#   else:
+#     await ctx.send(embed=embed)
 
-@client.command(name='qr')
-async def draw(ctx, *text):
-  from draw import draw
-  embed = draw(ctx, text)
-  if type(embed) == str:
-    await ctx.send(embed)
-  else:
-    await ctx.send(embed=embed)
+# @client.command(name='qr')
+# async def draw(ctx, *text):
+#   from draw import draw
+#   embed = draw(ctx, text)
+#   if type(embed) == str:
+#     await ctx.send(embed)
+#   else:
+#     await ctx.send(embed=embed)
 
 
 ##############################################################################################################################
