c92148d94e6222e2a86ce5e81f5df7a770154e22
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-12-28 11:34:07 +0900

    Update gacha.py

--- gacha.py
+++ gacha.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def get_gacha_list(worth):
     worth = int(worth)
-    incre = [0,0.5,2,4,10,50,100]
+    incre = [0,2,4,10,50,100]
     value = []
     for i in incre:
         value.append(int(round(worth*i)))
@@ -13,15 +13,14 @@ def get_gacha_list(worth):
 def gacha(times):
     # times = 뽑기 횟수
     # worth = 상자 가격
-    incre = [0,0.5,2,4,10,50,100]
-    result = [0,0,0,0,0,0,0,0,0,0]
+    incre = [0,2,4,10,50,100]
+    result = [0,0,0,0,0,0,0,0,0]
 
     for i in range(times):
-        coin = np.random.choice(incre, 1, p=[0.4494,0.105,0.4,0.04,0.005,0.0005,0.0001])
+        coin = np.random.choice(incre, 1, p=[0.7794,0.175, 0.04, 0.005,  0.0005, 0.0001])
         where = incre.index(coin)
         result[where] += 1
     
     return result
-
 
 
