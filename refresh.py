a76f3d5e5c9fb109abf59671665ad103fd068ac5
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2022-01-05 13:08:19 +0900

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -1,55 +1,59 @@
-from firebase_admin import db
+import sqlite3
 from randoms import random_value
 from get_time import get_time
 
-def dic_in_now(dic, com, time):
-  for i in range(1, 11):
-    now = dic[i]['now']
-    if now == 1:
-      value = dic[i]['value']
-      rvalue = random_value(value)
-      if i == 10:
-        i = str(i)
-        ref = db.reference()
-        ref.child(com).child(i).child('now').set(0)
-        ref.child(com).child('1').child('time').set(time['clock'])
-        ref.child(com).child('1').child('value').set(rvalue)
-        ref.child(com).child('1').child('now').set(1)
+def update_stock(company, time):
+  date = time['date']
+  clock = time['clock']
 
-      elif i >= 1 and i < 10:
-        j = i + 1
-        i = str(i)
-        j = str(j)
-        ref = db.reference()
-        ref.child(com).child(i).child('now').set(0)
-        ref.child(com).child(j).child('time').set(time['clock'])
-        ref.child(com).child(j).child('value').set(rvalue)
-        ref.child(com).child(j).child('now').set(1)
+  name = company[0]
+  before_value = int(company[1])
+  value = random_value(before_value)
+  before_num = int(company[5])
+  num = before_num + 1 if not before_num == 270 else 1
+
+  conn = sqlite3.connect('{}.db'.format(name), isolation_level=None)
+  c = conn.cursor()
+  c.execute('UPDATE stock SET value=? WHERE num=?', (int(value), num))
+  c.execute('UPDATE stock SET date=? WHERE num=?', (date, num))
+  c.execute('UPDATE stock SET clock=? WHERE num=?', (clock, num))
+
+  conn = sqlite3.connect('stock.db', isolation_level=None)
+  c = conn.cursor()
+  c.execute('UPDATE stock SET value=? WHERE company=?', (int(value), name))
+  c.execute('UPDATE stock SET before=? WHERE company=?', (before_value, name))
+  c.execute('UPDATE stock SET date=? WHERE company=?', (date, name))
+  c.execute('UPDATE stock SET clock=? WHERE company=?', (clock, name))
+  c.execute('UPDATE stock SET num=? WHERE company=?', (num, name))
+
+      
 
 
 def refresh():
-    ref = db.reference()
-    dic = ref.get()
-    펭귄 = dic['pg'] # 펭귄 = {1 : {'time':'12:02', 'value' : 1500, 'now' = 0 or 1}, 2 : {...}, ...}
-    시네 = dic['sn']
-    마코 = dic['mk']
-    윤아 = dic['ua']
-    냐룽 = dic['nl']
-    판다 = dic['pd']
-    가온 = dic['go']
-    티칩 = dic['tc']
-    시루 = dic['sl']
-    코어 = dic['ce']
+    conn = sqlite3.connect('stock.db', isolation_level=None)
+    c = conn.cursor()
+    meta = c.execute('SELECT * FROM stock WHERE company=?', ('meta',))
+    meta = meta.fetchone()
+    didim = c.execute('SELECT * FROM stock WHERE company=?', ('didim',))
+    didim = didim.fetchone()
+    gonglyoug = c.execute('SELECT * FROM stock WHERE company=?', ('gonglyoug',))
+    gonglyoug = gonglyoug.fetchone()
+    nuli = c.execute('SELECT * FROM stock WHERE company=?', ('nuli',))
+    nuli = nuli.fetchone()
+    hangil = c.execute('SELECT * FROM stock WHERE company=?', ('hangil',))
+    hangil = hangil.fetchone()
+    singom = c.execute('SELECT * FROM stock WHERE company=?', ('singom',))
+    singom = singom.fetchone()
 
     time = get_time()
 
-    dic_in_now(펭귄, 'pg', time)
-    dic_in_now(시네, 'sn', time)
-    dic_in_now(마코, 'mk', time)
-    dic_in_now(윤아, 'ua', time)
-    dic_in_now(냐룽, 'nl', time)
-    dic_in_now(판다, 'pd', time)
-    dic_in_now(가온, 'go', time)
-    dic_in_now(티칩, 'tc', time)
-    dic_in_now(시루, 'sl', time)
-    dic_in_now(코어, 'ce', time)
+    update_stock(meta, time)
+    update_stock(didim, time)
+    update_stock(gonglyoug, time)
+    update_stock(nuli, time)
+    update_stock(hangil, time)
+    update_stock(singom, time)
+
+
+
+    
