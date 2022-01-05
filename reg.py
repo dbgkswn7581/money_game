01e470218f0f12f0bfa449b6bbc2a8747af4bebf
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2022-01-05 13:11:23 +0900

    Update reg.py

--- reg.py
+++ reg.py
@@ -1,5 +1,5 @@
-from firebase_admin import db
 import discord
+import sqlite3
 
 def reg(ctx, text):
     user_keys = []
@@ -7,34 +7,20 @@ def reg(ctx, text):
     nickname = txt
     user_id = ctx.author.id
     
-    ref = db.reference()
-    dic = ref.get()
-    del dic['ce']
-    del dic['go']
-    del dic['mk']
-    del dic['nl']
-    del dic['pd']
-    del dic['pg']
-    del dic['sl']
-    del dic['sn']
-    del dic['tc']
-    del dic['ua']
-    del dic['admin']
-    del dic['samsung']
-    del dic['kakao']
-    del dic['naver']
-    del dic['korean']
-    del dic['kolon']
-    del dic['hyundai']
+    conn = sqlite3.connect("user.db", isolation_level=None)
+    c = conn.cursor()
     
-    for key in dic.keys():
-        user_keys.append(key)
+    c.execute('SELECT id,nickname FROM user')
+    user_s = c.fetchall()
+    user_keys = []
+    for i in user_s:
+        user_keys.append(i[0])
 
     if str(user_id) in user_keys:
-        ref = db.reference()
-        data = ref.child(str(user_id)).get()
-        nick = data['nickname']
-        money = data['money']
+        c.execute('SELECT * FROM user WHERE id=?', (str(user_id),))
+        data = c.fetchone()
+        nick = data[1]
+        money = data[2]
         embed = discord.Embed(title ="회원가입 실패",
         description = 'Money Game', color = discord.Color.red()
         )
@@ -43,29 +29,10 @@ def reg(ctx, text):
         return embed
     
     elif str(user_id) not in user_keys:
-        ref= db.reference()
-        dic = ref.get()
-        del dic['ce']
-        del dic['go']
-        del dic['mk']
-        del dic['nl']
-        del dic['pd']
-        del dic['pg']
-        del dic['sl']
-        del dic['sn']
-        del dic['tc']
-        del dic['ua']
-        del dic['admin']
-        del dic['samsung']
-        del dic['kakao']
-        del dic['naver']
-        del dic['korean']
-        del dic['kolon']
-        del dic['hyundai']
         nicks = []
 
-        for i in user_keys:
-            nicks.append(dic[str(i)]['nickname'])
+        for i in user_s:
+            nicks.append(i[1])
         
         if nickname in nicks:
             embed = discord.Embed(title ="회원가입 실패",
@@ -76,63 +43,36 @@ def reg(ctx, text):
             embed = discord.Embed(title = "%s님 반갑습니다!" %txt,
             description = "Money Game", color = discord.Color.green()
             )
-            embed.add_field(name='보유 자금', value='5000', inline=True)
+            embed.add_field(name='보유 자금', value='1,000,000', inline=True)
             embed.add_field(name="`$도움말`을 통해 사용 가능한 명령어를 확인해보세요" , value='접두사 : `$`', inline=True)
-            # embed.set_thumbnail(url="https://cravatar.eu/helmhead/%s/68.png" %txt)
 
-        ref = db.reference()
-        ref.update({'%s'%user_id:{
-            'nickname':nickname, 
-            'money':5000, 
-            'box':{'get' : 0, 'time' : 0, 'lost' : 0},
-            'pg':{'amount' : 0, 'value' : 0}, 
-            'sn':{'amount' : 0, 'value' : 0},
-            'mk':{'amount' : 0, 'value' : 0},
-            'ua':{'amount' : 0, 'value' : 0},
-            'nl':{'amount' : 0, 'value' : 0},
-            'pd':{'amount' : 0, 'value' : 0},
-            'go':{'amount' : 0, 'value' : 0},
-            'tc':{'amount' : 0, 'value' : 0},
-            'sl':{'amount' : 0, 'value' : 0},
-            'ce':{'amount' : 0, 'value' : 0}}})
-        
+        c.execute('INSERT INTO user(id, nickname, money, choose, stock) \
+            VALUES(?, ?, ?, ?, ?)', \
+                (str(user_id), txt, 1000000, 1,'0&0&0&0&0&0&0&0&0&0'))
+
         return embed
 
 def unreg(ctx):
     user_keys = []
     user_id = ctx.author.id
 
-    ref = db.reference()
-    dic = ref.get()
-    del dic['ce']
-    del dic['go']
-    del dic['mk']
-    del dic['nl']
-    del dic['pd']
-    del dic['pg']
-    del dic['sl']
-    del dic['sn']
-    del dic['tc']
-    del dic['ua']
-    del dic['admin']
-    del dic['samsung']
-    del dic['kakao']
-    del dic['naver']
-    del dic['korean']
-    del dic['kolon']
-    del dic['hyundai']
+    conn = sqlite3.connect("user.db", isolation_level=None)
+    c = conn.cursor()
 
-    for key in dic.keys():
-        user_keys.append(key)
+    c.execute('SELECT id,nickname FROM user')
+    user_s = c.fetchall()
+    user_keys = []
+    for i in user_s:
+        user_keys.append(i[0])
 
     if str(user_id) in user_keys:
-        ref = db.reference()
-        data = ref.child(str(user_id)).get()
-        nick = data['nickname']
-        money = data['money']
+        c.execute('SELECT * FROM user WHERE id=?', (str(user_id),))
+        data = c.fetchone()
+        nick = data[1]
+        money = data[2]
 
-        ref = db.reference()
-        ref.child(str(user_id)).delete()
+        c.execute('DELETE FROM user WHERE id=?', (str(user_id),))
+
 
         embed = discord.Embed(title ="Money Game 탈퇴",
         description = 'Money Game', color = discord.Color.dark_green()
