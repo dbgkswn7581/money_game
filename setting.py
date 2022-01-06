35dbd8c809a1523e5df040ee00ce969b9933cfd6
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2022-01-06 19:20:01 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,12 +1,10 @@
-import sqlite3
+import psycopg2
 
 def setting(ctx, text):
     user_id = ctx.author.id
-    
-    
 
     if text == ():
-        ctx_text = '`$설정 주식 (번호)` : \n(번호)에 1을 입력 시 모든 주식 정보가 표시되고, \n2를 입력 시 NNSTG 주식 정보만 표시되고, \n3을 입력 시 KOSPI 주식 정보만 표시됩니다.'
+        ctx_text = '`$설정 주식 (번호)` : \n(번호)에 1을 입력 시 모든 주식 정보가 표시되고, \n2를 입력 시 JJISI 주식 정보만 표시되고, \n3을 입력 시 KOSPI 주식 정보만 표시됩니다.'
         return ctx_text
 
     elif len(text) == 1:
@@ -20,23 +18,32 @@ def setting(ctx, text):
 
         if order == '주식' or order == 'wt' or order == 'ㅈㅅ' or order == 'stock':
             if choose == '1':
-                conn = sqlite3.connect('user.db', isolation_level=None)
+                conn = psycopg2.connect(host='ec2-3-209-234-80.compute-1.amazonaws.com',dbname='d8sv37cbum5a7k',user='kyshvxsusgztbc',password='938df8636f301f7656f277c4e2684ff5fbaba1fa68822cd73785e33c7bea62f2',port=5432)
                 c = conn.cursor()
-                c.execute('UPDATE user SET choose=? WHERE id=?', (1,str(user_id)))
+                c.execute('UPDATE "user" SET choose=%s WHERE id=%s', (1,str(user_id)))
+                conn.commit()
+                c.close()
+                conn.close()
 
                 ctx_text = '이제부터 `모든 주식` 정보가 표시됩니다.'
                 return ctx_text
 
             elif choose == '2':
-                conn = sqlite3.connect('user.db', isolation_level=None)
+                conn = psycopg2.connect(host='ec2-3-209-234-80.compute-1.amazonaws.com',dbname='d8sv37cbum5a7k',user='kyshvxsusgztbc',password='938df8636f301f7656f277c4e2684ff5fbaba1fa68822cd73785e33c7bea62f2',port=5432)
                 c = conn.cursor()
-                c.execute('UPDATE user SET choose=? WHERE id=?', (2,str(user_id)))
+                c.execute('UPDATE "user" SET choose=%s WHERE id=%s', (2,str(user_id)))
+                conn.commit()
+                c.close()
+                conn.close()
                 ctx_text = '이제부터 `NNSTG` 정보만 표시됩니다.'
                 return ctx_text
 
             elif choose == '3':
-                conn = sqlite3.connect('user.db', isolation_level=None)
+                conn = psycopg2.connect(host='ec2-3-209-234-80.compute-1.amazonaws.com',dbname='d8sv37cbum5a7k',user='kyshvxsusgztbc',password='938df8636f301f7656f277c4e2684ff5fbaba1fa68822cd73785e33c7bea62f2',port=5432)
                 c = conn.cursor()
-                c.execute('UPDATE user SET choose=? WHERE id=?', (3,str(user_id)))
+                c.execute('UPDATE "user" SET choose=%s WHERE id=%s', (3,str(user_id)))
+                conn.commit()
+                c.close()
+                conn.close()
                 ctx_text = '이제부터 `KOSPI` 정보만 표시됩니다.'
                 return ctx_text
