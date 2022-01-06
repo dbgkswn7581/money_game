e51804b2a9d0de8b487dd894036cccdaa1167214
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2022-01-06 19:19:30 +0900

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -1,4 +1,4 @@
-import sqlite3
+import psycopg2
 from randoms import random_value
 from get_time import get_time
 
@@ -12,38 +12,47 @@ def update_stock(company, time):
   before_num = int(company[5])
   num = before_num + 1 if not before_num == 270 else 1
 
-  conn = sqlite3.connect('{}.db'.format(name), isolation_level=None)
+  conn = psycopg2.connect(host='ec2-3-209-234-80.compute-1.amazonaws.com',dbname='d8sv37cbum5a7k',user='kyshvxsusgztbc',password='938df8636f301f7656f277c4e2684ff5fbaba1fa68822cd73785e33c7bea62f2',port=5432)
   c = conn.cursor()
-  c.execute('UPDATE stock SET value=? WHERE num=?', (int(value), num))
-  c.execute('UPDATE stock SET date=? WHERE num=?', (date, num))
-  c.execute('UPDATE stock SET clock=? WHERE num=?', (clock, num))
+  c.execute('UPDATE {} SET value=? WHERE num=%s'.format(name), (int(value), num))
+  c.execute('UPDATE {} SET date=? WHERE num=%s'.format(name), (date, num))
+  c.execute('UPDATE {} SET clock=? WHERE num=%s'.format(name), (clock, num))
+  conn.commit()
+  c.close()
+  conn.close()
 
-  conn = sqlite3.connect('stock.db', isolation_level=None)
+  conn = psycopg2.connect(host='ec2-3-209-234-80.compute-1.amazonaws.com',dbname='d8sv37cbum5a7k',user='kyshvxsusgztbc',password='938df8636f301f7656f277c4e2684ff5fbaba1fa68822cd73785e33c7bea62f2',port=5432)
   c = conn.cursor()
-  c.execute('UPDATE stock SET value=? WHERE company=?', (int(value), name))
-  c.execute('UPDATE stock SET before=? WHERE company=?', (before_value, name))
-  c.execute('UPDATE stock SET date=? WHERE company=?', (date, name))
-  c.execute('UPDATE stock SET clock=? WHERE company=?', (clock, name))
-  c.execute('UPDATE stock SET num=? WHERE company=?', (num, name))
+  c.execute('UPDATE stock SET value=%s WHERE company=%s', (int(value), name))
+  c.execute('UPDATE stock SET before=%s WHERE company=%s', (before_value, name))
+  c.execute('UPDATE stock SET date=%s WHERE company=%s', (date, name))
+  c.execute('UPDATE stock SET clock=%s WHERE company=%s', (clock, name))
+  c.execute('UPDATE stock SET num=%s WHERE company=%s', (num, name))
+  conn.commit()
+  c.close()
+  conn.close()
 
       
 
 
 def refresh():
-    conn = sqlite3.connect('stock.db', isolation_level=None)
+    conn = psycopg2.connect(host='ec2-3-209-234-80.compute-1.amazonaws.com',dbname='d8sv37cbum5a7k',user='kyshvxsusgztbc',password='938df8636f301f7656f277c4e2684ff5fbaba1fa68822cd73785e33c7bea62f2',port=5432)
     c = conn.cursor()
-    meta = c.execute('SELECT * FROM stock WHERE company=?', ('meta',))
-    meta = meta.fetchone()
-    didim = c.execute('SELECT * FROM stock WHERE company=?', ('didim',))
-    didim = didim.fetchone()
-    gonglyoug = c.execute('SELECT * FROM stock WHERE company=?', ('gonglyoug',))
-    gonglyoug = gonglyoug.fetchone()
-    nuli = c.execute('SELECT * FROM stock WHERE company=?', ('nuli',))
-    nuli = nuli.fetchone()
-    hangil = c.execute('SELECT * FROM stock WHERE company=?', ('hangil',))
-    hangil = hangil.fetchone()
-    singom = c.execute('SELECT * FROM stock WHERE company=?', ('singom',))
-    singom = singom.fetchone()
+    c.execute('SELECT * FROM stock WHERE company=%s', ('meta',))
+    meta = c.fetchone()
+    c.execute('SELECT * FROM stock WHERE company=%s', ('didim',))
+    didim = c.fetchone()
+    c.execute('SELECT * FROM stock WHERE company=%s', ('gonglyoug',))
+    gonglyoug = c.fetchone()
+    c.execute('SELECT * FROM stock WHERE company=%s', ('nuli',))
+    nuli = c.fetchone()
+    c.execute('SELECT * FROM stock WHERE company=%s', ('hangil',))
+    hangil = c.fetchone()
+    c.execute('SELECT * FROM stock WHERE company=%s', ('singom',))
+    singom = c.fetchone()
+
+    c.close()
+    conn.close()
 
     time = get_time()
 
